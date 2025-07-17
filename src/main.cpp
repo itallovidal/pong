81683d672203089ea6c1df95c21534f3f3989511
itallovidal <itallovidalll@gmail.com>
2025-07-17 20:34:18 -0300

    add title, handling text

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+
+#define WELCOME_MESSAGE "Bem vindo ao Porongo Pong"
+
+sf::Text titleComponent(sf::Font font, sf::RenderWindow *window){
+    sf::Text text(font);
+    text.setString(WELCOME_MESSAGE);
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+
+    float textSize = text.getLocalBounds().size.x;
+    float centerScreen = window->getSize().x / 2;
+    float halfText = floor(textSize / 2);
+
+    text.setPosition({centerScreen - halfText , 10});
+
+    return text;
+}
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800, 600u}), "Porongo Pong");
     window.setFramerateLimit(144);
+    
+    sf::Font font;
+    if (!font.openFromFile("src/assets/roboto.ttf"))
+    {
+        std::cout << "Error importing font" << "\n" ;
+        return 1;
+    }
+
 
     while (window.isOpen())
     {
@@ -16,6 +43,9 @@ int main()
         }
 
         window.clear();
+
+        window.draw(titleComponent(font, &window));
+
         window.display();
     }
 }
